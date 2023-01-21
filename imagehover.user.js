9a74018ec50c5ee436874613b2fe995d865a1959
xrez <fezthrowaway1@gmail.com>
2023-01-22 00:26:36 +0200

    links work, just need to write checks for filetype

--- imagehover.user.js
+++ imagehover.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         gelbooru image hover
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/xrez1
 // @version      0.1
 // @description  will show full image when hovering over thumbnails
 // @author       xrez
@@ -13,13 +13,23 @@
 (function() {
     'use strict';
 
+    //https://gelbooru.com/thumbnails/9b/d8/thumbnail_9bd85f018ace5382aba1e5b63643a8aa.jpg
+    //https://img3.gelbooru.com//images/9b/d8/9bd85f018ace5382aba1e5b63643a8aa.jpeg
+
+    function getFullImg(src){
+        var x = src
+        var y = x.replace("thumbnails", "/images");
+        var z = y.replace("thumbnail_", "");
+        var q = z.replace("gelbooru.com","img3.gelbooru.com");
+        return q;
+    }
+
     $(".thumbnail-preview").hover(function () {
-        print(this.$("img"))
+        const obj =  $(this).find("img");
+        var thumbLink = Object.values(obj)[0].src;
+        console.log(getFullImg(thumbLink));
+
         }, function () {
         }
     );
-
-
-
-    // Your code here...
 })();
